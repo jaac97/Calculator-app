77da20f611581fa1e98676172e517c780d339d79
Josué Alarcón <josuealarco@gmail.com>
2022-07-20 16:24:06 -0500

    Adjusting Bar theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const {watch, dest, src, series} = require('gulp');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
+const autoprefixer = require('gulp-autoprefixer')
 function buildStyles() {
     return gulp.src('./build/sass/**/*.scss')
       .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        cascade: false
+        }))
       .pipe(gulp.dest('./css'));
 };
 
