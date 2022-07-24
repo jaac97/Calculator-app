a65944e53284f45031f237cc3a2fa19b06aa7242
Josué Alarcón <josuealarco@gmail.com>
2022-07-23 20:22:53 -0500

    Suma finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let quantities = [];
+let result = 0;
+let op = '';
 
 const startapp = () => {
     document.addEventListener('DOMContentLoaded', app)
@@ -11,133 +14,310 @@ const app = () => {
         localStorage.setItem('calculator_token', JSON.stringify(e.target.value))
         changePreferences(parseInt(e.target.value))
     });
+
+    const btns = document.querySelectorAll('.calc__btn');
+    btns.forEach(btn => btn.addEventListener('click', () => {
+        operations(btn.value)
+    }))
 }
 
 const changePreferences = value => {
     const body = document.querySelector('body'),
-    calcTop = document.querySelector('.calc__top'),
-    calcOptions =document.querySelector('.calc__options'),
-    calcThemeRange = document.querySelector('.calc__theme-range'),
-    calcScreen = document.querySelector('.calc__screen')
+        calcTop = document.querySelector('.calc__top'),
+        calcOptions = document.querySelector('.calc__options'),
+        calcThemeRange = document.querySelector('.calc__theme-range'),
+        calcScreen = document.querySelector('.calc__screen')
     calcButtons = document.querySelector('.calc__buttons')
     calcBtn = document.querySelectorAll('.calc__btn'),
-    calcBtnSpecial = document.querySelectorAll('.calc__btn--special');
-    calcBtnSpecialEqual =document.querySelector('.calc__btn--special-equal');
+        calcBtnSpecial = document.querySelectorAll('.calc__btn--special');
+    calcBtnSpecialEqual = document.querySelector('.calc__btn--special-equal');
 
     switch (value) {
         case 1:
             // Remove Styles
-            body.classList.remove('body-2');
-            calcTop.classList.remove('calc__top-2');
-            calcOptions.classList.remove('calc__options-2');
-            calcThemeRange.classList.remove('calc__theme-range-2');
-            calcScreen.classList.remove('calc__screen-2');
-            calcButtons.classList.remove('calc__buttons-2');
+            body
+                .classList
+                .remove('body-2');
+            calcTop
+                .classList
+                .remove('calc__top-2');
+            calcOptions
+                .classList
+                .remove('calc__options-2');
+            calcThemeRange
+                .classList
+                .remove('calc__theme-range-2');
+            calcScreen
+                .classList
+                .remove('calc__screen-2');
+            calcButtons
+                .classList
+                .remove('calc__buttons-2');
             calcBtn.forEach(btn => btn.classList.remove('calc__btn-2'));
             calcBtnSpecial.forEach(btn => btn.classList.remove('calc__btn--special-2'));
-            calcBtnSpecialEqual.classList.remove('calc__btn--special-equal-2');
+            calcBtnSpecialEqual
+                .classList
+                .remove('calc__btn--special-equal-2');
 
-            body.classList.remove('body-3');
-            calcTop.classList.remove('calc__top-3');
-            calcOptions.classList.remove('calc__options-3');
-            calcThemeRange.classList.remove('calc__theme-range-3');
-            calcScreen.classList.remove('calc__screen-3');
-            calcButtons.classList.remove('calc__buttons-3');
+            body
+                .classList
+                .remove('body-3');
+            calcTop
+                .classList
+                .remove('calc__top-3');
+            calcOptions
+                .classList
+                .remove('calc__options-3');
+            calcThemeRange
+                .classList
+                .remove('calc__theme-range-3');
+            calcScreen
+                .classList
+                .remove('calc__screen-3');
+            calcButtons
+                .classList
+                .remove('calc__buttons-3');
             calcBtn.forEach(btn => btn.classList.remove('calc__btn-3'));
             calcBtnSpecial.forEach(btn => btn.classList.remove('calc__btn--special-3'));
-            calcBtnSpecialEqual.classList.remove('calc__btn--special-equal-3');
+            calcBtnSpecialEqual
+                .classList
+                .remove('calc__btn--special-equal-3');
 
             // Add Styles
-            body.classList.add('body-1');
-            calcTop.classList.add('calc__top-1');
-            calcOptions.classList.add('calc__options-1');
-            calcThemeRange.classList.add('calc__theme-range-1');
-            calcScreen.classList.add('calc__screen-1');
-            calcButtons.classList.add('calc__buttons-1');
+            body
+                .classList
+                .add('body-1');
+            calcTop
+                .classList
+                .add('calc__top-1');
+            calcOptions
+                .classList
+                .add('calc__options-1');
+            calcThemeRange
+                .classList
+                .add('calc__theme-range-1');
+            calcScreen
+                .classList
+                .add('calc__screen-1');
+            calcButtons
+                .classList
+                .add('calc__buttons-1');
             calcBtn.forEach(btn => btn.classList.add('calc__btn-1'));
             calcBtnSpecial.forEach(btn => btn.classList.add('calc__btn--special-1'));
-            calcBtnSpecialEqual.classList.add('calc__btn--special-equal-1');
-        break;
+            calcBtnSpecialEqual
+                .classList
+                .add('calc__btn--special-equal-1');
+            break;
         case 2:
             // Removing Styles
-            body.classList.remove('body-1');
-            calcTop.classList.remove('calc__top-1');
-            calcOptions.classList.remove('calc__options-1');
-            calcThemeRange.classList.remove('calc__theme-range-1');
-            calcScreen.classList.remove('calc__screen-1');
-            calcButtons.classList.remove('calc__buttons-1');
+            body
+                .classList
+                .remove('body-1');
+            calcTop
+                .classList
+                .remove('calc__top-1');
+            calcOptions
+                .classList
+                .remove('calc__options-1');
+            calcThemeRange
+                .classList
+                .remove('calc__theme-range-1');
+            calcScreen
+                .classList
+                .remove('calc__screen-1');
+            calcButtons
+                .classList
+                .remove('calc__buttons-1');
             calcBtn.forEach(btn => btn.classList.remove('calc__btn-1'));
             calcBtnSpecial.forEach(btn => btn.classList.remove('calc__btn--special-1'));
-            calcBtnSpecialEqual.classList.remove('calc__btn--special-equal-1');
+            calcBtnSpecialEqual
+                .classList
+                .remove('calc__btn--special-equal-1');
 
-
-
-            body.classList.remove('body-3');
-            calcTop.classList.remove('calc__top-3');
-            calcOptions.classList.remove('calc__options-3');
-            calcThemeRange.classList.remove('calc__theme-range-3');
-            calcScreen.classList.remove('calc__screen-3');
-            calcButtons.classList.remove('calc__buttons-3');
+            body
+                .classList
+                .remove('body-3');
+            calcTop
+                .classList
+                .remove('calc__top-3');
+            calcOptions
+                .classList
+                .remove('calc__options-3');
+            calcThemeRange
+                .classList
+                .remove('calc__theme-range-3');
+            calcScreen
+                .classList
+                .remove('calc__screen-3');
+            calcButtons
+                .classList
+                .remove('calc__buttons-3');
             calcBtn.forEach(btn => btn.classList.remove('calc__btn-3'));
             calcBtnSpecial.forEach(btn => btn.classList.remove('calc__btn--special-3'));
-            calcBtnSpecialEqual.classList.remove('calc__btn--special-equal-3');
-
+            calcBtnSpecialEqual
+                .classList
+                .remove('calc__btn--special-equal-3');
 
             // Add styles
-            body.classList.add('body-2');
-            calcTop.classList.add('calc__top-2');
-            calcOptions.classList.add('calc__options-2');
-            calcThemeRange.classList.add('calc__theme-range-2');
-            calcScreen.classList.add('calc__screen-2');
-            calcButtons.classList.add('calc__buttons-2');
+            body
+                .classList
+                .add('body-2');
+            calcTop
+                .classList
+                .add('calc__top-2');
+            calcOptions
+                .classList
+                .add('calc__options-2');
+            calcThemeRange
+                .classList
+                .add('calc__theme-range-2');
+            calcScreen
+                .classList
+                .add('calc__screen-2');
+            calcButtons
+                .classList
+                .add('calc__buttons-2');
             calcBtn.forEach(btn => btn.classList.add('calc__btn-2'));
             calcBtnSpecial.forEach(btn => btn.classList.add('calc__btn--special-2'));
-            calcBtnSpecialEqual.classList.add('calc__btn--special-equal-2');
-        break;
+            calcBtnSpecialEqual
+                .classList
+                .add('calc__btn--special-equal-2');
+            break;
         case 3:
             // Removing Styles
-            body.classList.remove('body-1');
-            calcTop.classList.remove('calc__top-1');
-            calcOptions.classList.remove('calc__options-1');
-            calcThemeRange.classList.remove('calc__theme-range-1');
-            calcScreen.classList.remove('calc__screen-1');
-            calcButtons.classList.remove('calc__buttons-1');
+            body
+                .classList
+                .remove('body-1');
+            calcTop
+                .classList
+                .remove('calc__top-1');
+            calcOptions
+                .classList
+                .remove('calc__options-1');
+            calcThemeRange
+                .classList
+                .remove('calc__theme-range-1');
+            calcScreen
+                .classList
+                .remove('calc__screen-1');
+            calcButtons
+                .classList
+                .remove('calc__buttons-1');
             calcBtn.forEach(btn => btn.classList.remove('calc__btn-1'));
             calcBtnSpecial.forEach(btn => btn.classList.remove('calc__btn--special-1'));
-            calcBtnSpecialEqual.classList.remove('calc__btn--special-equal-1');
+            calcBtnSpecialEqual
+                .classList
+                .remove('calc__btn--special-equal-1');
 
-
-
-            body.classList.remove('body-2');
-            calcTop.classList.remove('calc__top-2');
-            calcOptions.classList.remove('calc__options-2');
-            calcThemeRange.classList.remove('calc__theme-range-2');
-            calcScreen.classList.remove('calc__screen-2');
-            calcButtons.classList.remove('calc__buttons-2');
+            body
+                .classList
+                .remove('body-2');
+            calcTop
+                .classList
+                .remove('calc__top-2');
+            calcOptions
+                .classList
+                .remove('calc__options-2');
+            calcThemeRange
+                .classList
+                .remove('calc__theme-range-2');
+            calcScreen
+                .classList
+                .remove('calc__screen-2');
+            calcButtons
+                .classList
+                .remove('calc__buttons-2');
             calcBtn.forEach(btn => btn.classList.remove('calc__btn-2'));
             calcBtnSpecial.forEach(btn => btn.classList.remove('calc__btn--special-2'));
-            calcBtnSpecialEqual.classList.remove('calc__btn--special-equal-2');
-
+            calcBtnSpecialEqual
+                .classList
+                .remove('calc__btn--special-equal-2');
 
             // Add styles
-            body.classList.add('body-3');
-            calcTop.classList.add('calc__top-3');
-            calcOptions.classList.add('calc__options-3');
-            calcThemeRange.classList.add('calc__theme-range-3');
-            calcScreen.classList.add('calc__screen-3');
-            calcButtons.classList.add('calc__buttons-3');
+            body
+                .classList
+                .add('body-3');
+            calcTop
+                .classList
+                .add('calc__top-3');
+            calcOptions
+                .classList
+                .add('calc__options-3');
+            calcThemeRange
+                .classList
+                .add('calc__theme-range-3');
+            calcScreen
+                .classList
+                .add('calc__screen-3');
+            calcButtons
+                .classList
+                .add('calc__buttons-3');
             calcBtn.forEach(btn => btn.classList.add('calc__btn-3'));
             calcBtnSpecial.forEach(btn => btn.classList.add('calc__btn--special-3'));
-            calcBtnSpecialEqual.classList.add('calc__btn--special-equal-3');
-    
+            calcBtnSpecialEqual
+                .classList
+                .add('calc__btn--special-equal-3');
+
         default:
             break;
     }
 }
 
+const operations = (value) => {
+    const calcResult = document.querySelector('.calc__results');
+    if(value == 'reset'){
+        calcResult.textContent = 0;
+        result=0;
+        op='';
+        quantities=[];
+    }else if (value == 'del') {
+        calcResult.textContent = calcResult.textContent.slice(0,-1)
+        if( calcResult.textContent === '') {
+            calcResult.textContent = 0;
+        }
+    }else if(typeof parseInt(value) && !isNaN(parseInt(value))){
+        if(calcResult.textContent == '0'){
+            calcResult.textContent = parseInt(value)
+        }else{
+            calcResult.textContent += ' ' + parseInt(value)
+        }
+    }else if(value == '+' || value == '='){
+        if(op === ''){
+            quantities = [...quantities, parseInt(calcResult.textContent)]
+            op = value;
+            calcResult.textContent+= ' ' + value;
+        }else if(value === '+') {
 
+           if(calcResult.textContent.slice(-1) != value){
 
+            quantities = calcResult.textContent.split('+')
+            calcResult.textContent+= ' ' + value ;
+            
+            if(quantities.length > 1){
+                quantities.forEach(n => {
+                    result +=  parseInt(n);
+                })
+                quantities = [];
+                result = 0;
+            }
 
+           }
+          
+        }else if(value === '='){ 
 
+            quantities = calcResult.textContent.split(op)
+
+            quantities.forEach(n => {
+                result += parseInt(n)
+            })
+            op = ''
+            
+            quantities =[]
+            calcResult.textContent = result;
+            result=0
+        }
+    }
+
+}
 
 startapp()
